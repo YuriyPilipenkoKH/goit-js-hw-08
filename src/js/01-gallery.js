1c73e886e068200ecc6d6b840a1c27dcb102a7ec
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-21 15:48:19 +0200

    vid

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,20 +1,18 @@
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
 
-const gal = document.querySelector('.gallery')
+const gall = document.querySelector('.gallery');
 
+const myMarkup =  galleryItems.map(item => `<a class="gallery__item" href="${item.original}" >
+<img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}">
+</a>`).join('');
 
-const myMarkup =  galleryItems.map(item => `<a class="gallery__link" href="${item.original}" >
-<img class="gallery__image" src="${item.preview}" data-src="${item.original}" alt="${item.description}">
-</a>`).join('')
-
-
-gal.insertAdjacentHTML('afterbegin', myMarkup)
+gall.insertAdjacentHTML('afterbegin', myMarkup);
 
 const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
