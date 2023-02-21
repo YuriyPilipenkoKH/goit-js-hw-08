66f14d0cf6b13fd6fe181174ac313325d0015d4f
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-21 14:25:39 +0200

    gal

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,21 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gal = document.querySelector('.gallery')
+
+
+const myMarkup =  galleryItems.map(item => `<a class="gallery__link" href="${item.original}" >
+<img class="gallery__image" src="${item.preview}" data-src="${item.original}" alt="${item.description}">
+</a>`).join('')
+
+
+gal.insertAdjacentHTML('afterbegin', myMarkup)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
